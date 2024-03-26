56d28d094e8c7902a230cc5a63660b5afa2284f0
Elhoucine lazrak <142110120+elazrk@users.noreply.github.com>
2024-03-26 00:14:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -114,4 +114,26 @@ resource "aws_nat_gateway" "nat_gateway" {
   tags = {
     Name = "demo_nat_gateway"
   }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "mybucket"
+}
+
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_object_lock_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    default_retention {
+      mode = "COMPLIANCE"
+      days = 5
+    }
+  }
 }
